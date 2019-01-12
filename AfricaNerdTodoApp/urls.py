875b236a50f4c909419a503cdd15df8f86bc471c
Africa Nerd <34450905+africanerd@users.noreply.github.com>
2019-01-12 07:43:07 -0500

    Add files via upload

--- AfricaNerdTodoApp/urls.py
+++ AfricaNerdTodoApp/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from . import views
 
 urlpatterns = [
-    #url(r'^$', include('todos.urls')),
+    url(r'^$', views.index),
     url(r'^todos/', include('AfricaNerdTodos.urls')),
     url(r'^admin/', admin.site.urls),
 ]
