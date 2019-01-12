a49dce005364445ddd734fd7b92b1a662e9c6e5e
Africa Nerd <34450905+africanerd@users.noreply.github.com>
2019-01-11 23:52:42 -0500

    Update settings.py

--- AfricaNerdTodoApp/settings.py
+++ AfricaNerdTodoApp/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'kznomr&xh)^8^x@j-vi^tggrt^w^v_k@m$uq!_x^o-&g(ay4b7'
+SECRET_KEY = 'ADD YOUR SECRECT KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
